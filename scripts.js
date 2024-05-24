77294d73182840a8cb8dfa06d026f747341477d9
Disego <disegorachelm@gmail.com>
2024-05-24 15:44:26 +0200

    Advanced exercise done

--- scripts.js
+++ scripts.js
@@ -52,9 +52,9 @@ const namesWithS = names.map(name => name.toUpperCase().includes("S"))
 console.log(namesWithS)
 
 // Exercise 7
-const nameAndProvince = names.reduce((add, name, index) => {
-  add[name] = provinces[index];
-  return add;
+const nameAndProvince = names.reduce((acc, name, index) => {
+  acc[name] = provinces[index];
+  return acc;
 }, {})
 
 console.log(nameAndProvince)
@@ -64,18 +64,52 @@ console.log(nameAndProvince)
 // Advanced Exercises
 
 // Exercise 1
-console.log(products.forEach(product => {return product.product}))
+console.log(products.map(product => product.product))
 
 // Exercise 2
-
+console.log(products.filter(({product}) => product.length > 5))
 
 // Exercise 3
-
+console.log(
+  products.filter(({price}) => {
+    const converted = parseInt(price);
+    return !isNaN(converted);
+  })
+  .map((product) => {
+    const numericalPrice = parseInt(product.price);
+    return { ...product, price: numericalPrice};
+  })
+  .reduce((acc, { price }) => {
+    return acc + price;
+  }, 0))
 
 // Exercise 4
-
+console.log(products.reduce((acc, { product }) => {
+  return acc + product;
+}, ''));
 
 // Exercise 5
+const highAndLow = products
+  .filter(({ price }) => {
+    const converted = parseInt(price);
+    return !isNaN(converted);
+  })
+  .map((product) => {
+    const numberPrice = parseInt(product.price);
+    return { ...product, price: numberPrice };
+  })
+  .reduce((result, { price }) => {
+    if (!result.highest || price > result.highest) {
+      result.highest = price;
+    }
+    if (!result.lowest || price < result.lowest) {
+      result.lowest = price;
+    }
+    return result;
+  }, {});
 
+console.log(`Highest: ${highAndLow.highest}. Lowest: ${highAndLow.lowest}.`);
 
 // Exercise 6
+
+
